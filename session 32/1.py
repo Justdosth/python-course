67a0759b73110e84c9329e6007a3a333e773d3ec
Justdosth <Hyuuga_neji1375@yahoo.com>
2022-04-03 13:52:26 +0430

    hw 32

---
+++ session 32/1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-3,3,0.25)
+y = np.arange(-2,2,0.25)
+X,Y = np.meshgrid(x,y)
+
+z1 = np.exp(-(X**2+Y**2))
+z2 = np.exp(-((X-1)**2+(Y-1)**2))
+Z = (z1-z2)**2
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(X,Y,Z)
+plt.show()
